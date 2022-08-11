986f57e171fd48444fe3aa7ab9a19102637da0f0
XiouYu <jami423520@gmail.com>
2022-08-11 18:40:30 +0800

    section main rwd ok

---
+++ js/all.js
@@ -0,0 +1,22 @@
+
+$(document).ready(function() {
+
+    $('.btn-top').click(function(e) {
+        (e).preventDefault();
+        $('html,body').animate({
+            scrollTop: 0
+        }, 1000);
+    })
+
+
+
+
+
+
+
+
+
+
+
+
+});
