f4bb518f5e4bcc56b657dea0380895c22d40a2ea
XiouYu <jami423520@gmail.com>
2022-08-17 15:44:02 +0800

    price ok

--- js/price.js
+++ js/price.js
@@ -7,5 +7,21 @@ $(document).ready(function() {
         }, 
         1000
         );
-    })
+    })    
 });
+
+
+var acc = document.getElementsByClassName("accordion-question");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+            var panel = this.nextElementSibling;
+        if (panel.style.display === "block") {
+            panel.style.display = "none";
+        } else {
+            panel.style.display = "block";
+        }
+    });
+}
