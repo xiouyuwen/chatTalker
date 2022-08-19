14db1cb8d78f086bd28ece1a76e5d054180118d8
XiouYu <jami423520@gmail.com>
2022-08-19 15:00:49 +0800

    feeback

--- js/index.js
+++ js/index.js
@@ -9,14 +9,38 @@ $(document).ready(function() {
     })
 });
 
-var swiper = new Swiper(".mySwiper", {
-    slidesPerView: 3,
-    spaceBetween: 30,
-    pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-    },
-});
-
-
-
+function initSwiper() {
+    const swiper = new Swiper("#comment-swiper", {
+        slidesPerView: 1,
+        /* 斷點設定 */
+        breakpoints: {
+            1440: {
+                slidesPerView: 3
+            },
+            767: {
+                slidesPerView: 2
+            },
+                /* 更小時都顯示 1 欄 */
+            0: {
+                slidesPerView: 1
+            }
+        },
+        
+            spaceBetween: 16,
+            pagination: {
+                el: ".swiper-pagination",
+                clickable: true,
+            },
+    });
+}
+initSwiper();
+// var swiper = new Swiper(".mySwiper", {
+//     
+//     },
+//         slidesPerView: 3,
+//         spaceBetween: 30,
+//         pagination: {
+//             el: ".swiper-pagination",
+//             clickable: true,
+//         },
+// });
